bdb88faf3dc2df04fd9269399809fd66bbcdf420
maicon vinicius <maiconvinicius7756@gmail.com>
2025-12-26 12:47:39 -0300

    Estruturação inicial da página de listagem de funcionários, vista pelo dirigente - criação da página em HTML básico

---
+++ listagens/listagem_funcionarios.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Listagem | Funcionários </title>
+    </head>
+
+    <body>
+
+        <header>
+
+            <div class="titulo">
+                <h1> CREATOR SYNC </h1>
+            </div>
+
+        </header>
+
+        <main>
+
+            <div class="subtitulo">
+                <h2> funcionários </h2>
+            </div>
+            
+            <section class="lista">
+
+                <ol>
+
+                </ol>
+            </section>
+            
+        </main>
+        
+    </body>
+    
+</html>
