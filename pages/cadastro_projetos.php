154b98c2ae12069ded8a8a1faf2e3f279ee8d085
maicon vinicius <maiconvinicius7756@gmail.com>
2026-01-16 19:51:15 -0300

    Estruturação, visual, final da página de cadastro de projetos - ajuste final no HTML - adição de toda a estilização em CSS - adiçaõ do link, de redirecionamento, para cadastrar projetos na página de redirecionamento do dirigente

--- pages/cadastro_projetos.php
+++ pages/cadastro_projetos.php
@@ -4,6 +4,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../imgs/keyframe_logo_a.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
+        <link rel="stylesheet" href="../css/style_cadastro_projetos.css">
         <title> Cadastro | Projetos </title>
     </head>
 
@@ -11,10 +14,7 @@
 
         <header>
             <div class="cabecalho">
-
-                <img src="">
-                <h1> CREATOR SYNC </h1>
-
+                <h1> CREATOR <span><i>SYNC</i></span></h1>
             </div>
             
         </header>
@@ -23,46 +23,51 @@
 
             <section class="cadastro">
 
-                <div class="titulo">
-                    <h3> projetos </h3>
-                </div>
-
                 <div class="formulario">
 
+                    <div class="titulo">
+                        <p><i> projetos. </i></p>
+                    </div>
+
                     <form action="../actions/cadastrandi_projeto.php" method="POST">
+                        <div class="dados-primarios">
+                          <select name="pedido" required>
 
-                        <select name="pedido" required>
+                                <option value="" selected disabled hidden> Pedido </option>
+                                <option value=""> X </option>
+                                <option value=""> Y </option>
+                                <option value=""> Z </option>
 
-                            <option value="" selected disabled hidden> Pedido </option>
-                            <option value=""> X </option>
-                            <option value=""> Y </option>
-                            <option value=""> Z </option>
+                            </select>
 
-                        </select>
+                            <input type="text" placeholder="Título" name="titulo" required readonly> 
+                            <input type="text" placeholder="Descrição" name="descricao" required readonly>
+                        </div>
 
-                        <input type="text" placeholder="Título" name="titulo" required readonly> 
-                        <input type="text" placeholder="Descrição" name="descricao" required readonly>
+                        <div class="dados-funcionarios">
+                            <select name="editor" required>
 
-                        <select name="editor" required>
+                                <option value="" selected disabled hidden> Editor </option>
+                                <option value=""> A </option>
+                                <option value=""> B </option>
+                                <option value=""> C </option>
 
-                            <option value="" selected disabled hidden> Editor </option>
-                            <option value=""> A </option>
-                            <option value=""> B </option>
-                            <option value=""> C </option>
+                            </select>
 
-                        </select>
+                            <select name="roteirista" required>
 
-                        <select name="roteirista" required>
+                                <option value="" selected disabled hidden> Roteirista </option>
+                                <option value=""> A </option>
+                                <option value=""> B </option>
+                                <option value=""> C </option>
 
-                            <option value="" selected disabled hidden> Roteirista </option>
-                            <option value=""> A </option>
-                            <option value=""> B </option>
-                            <option value=""> C </option>
-
-                        </select>
+                            </select>
+                        </div>
                         
-                        <input type="date" placeholder="Início" name="inicio" required>
-                        <input type="date" placeholder="Previsão" name="previsao" required>
+                        <div class="dados-datas">
+                            <input type="date" placeholder="Início" name="inicio" required>
+                            <input type="date" placeholder="Previsão" name="previsao" required>
+                        </div>
     
 
                         <div class="botoes">
@@ -75,6 +80,11 @@
                     </form>
 
 
+                </div>
+                
+                <div class="logos">
+                    <a href="../pages/landingpage_creatorsync.html" target="_blank"><img src="../imgs/keyframe_logo_a.png"></a>
+                    <img src="../imgs/creatorsync_logo_a.png">
                 </div>
                 
             </section>
