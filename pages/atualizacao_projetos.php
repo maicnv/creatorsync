b56c8612fec008b1160aca41b8d94841600f3fbc
maicon vinicius <maiconvinicius7756@gmail.com>
2026-01-17 13:16:01 -0300

    Estruturação, visual, final da página de atualização de projetos - ajuste final no HTML - adição de toda a estilização em CSS - adição do link, de redirecionamento, para cadastrar funcionarios na página de redirecionamento do dirigente - ajustes no visual da página de redirecionamento do dirigente

--- pages/atualizacao_projetos.php
+++ pages/atualizacao_projetos.php
@@ -4,6 +4,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" href="../imgs/keyframe_logo_a.png" type="image/x-icon">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap">
+        <link rel="stylesheet" href="../css/style_atualizacao_projetos.css">
         <title> Atualização | Projetos </title>
     </head>
 
@@ -11,10 +14,7 @@
 
         <header>
             <div class="cabecalho">
-
-                <img src="">
-                <h1> CREATOR SYNC </h1>
-
+                <h1> CREATOR <span><i>SYNC</span></i></h1>
             </div>
             
         </header>
@@ -23,30 +23,52 @@
 
             <section class="atualizacao">
 
-                <div class="titulo">
-                    <h3> projetos </h3>
-                </div>
-
                 <div class="formulario">
 
+                    <div class="titulo">
+                        <p><i> projetos. </i></p>
+                    </div>
+
                     <form action="../actions/atualizar_projeto.php" method="POST">
+                        <div class="dados-pedido">
+                            <select name="projeto" required>
 
-                        <select name="projeto" required>
+                                <option value="" selected disabled hidden> Projeto </option>
+                                <option value=""> X </option>
+                                <option value=""> Y </option>
+                                <option value=""> Z </option>
 
-                            <option value="" selected disabled hidden> Projeto </option>
-                            <option value=""> X </option>
-                            <option value=""> Y </option>
-                            <option value=""> Z </option>
+                            </select>
 
-                        </select>
+                            <input type="text" placeholder="Título" name="titulo" required readonly> 
+                            <input type="text" placeholder="Descrição" name="descricao" required readonly>
+                        </div>
 
-                        <input type="text" placeholder="Título" name="titulo" required readonly> 
-                        <input type="text" placeholder="Descrição" name="descricao" required readonly>
+                        <div class="dados-etapas">
 
-                        <input type="text" placeholder="Edição" name="edicao" required>
-                        <input type="text" placeholder="Roteiro" name="roteiro" required>
-                        <input type="text" placeholder="Caminho" name="caminho" required>
-                        <input type="date" name="entrega" required>
+                            <select name="edicao" required>
+
+                                <option value="" selected disabled hidden> Edição </option>
+                                <option value="desenvolvendo"> Desenvolvendo </option>
+                                <option value="analisando"> Analisando </option>
+                                <option value="aprovado"> Aprovado </option>
+
+                            </select>
+
+                            <select name="Roteiro" required>
+                                <option value="" selected disabled hidden> Roteiro </option>
+                                <option value="desenvolvendo"> Desenvolvendo </option>
+                                <option value="analisando"> Analisando </option>
+                                <option value="aprovado"> Aprovado </option>
+                                
+                            </select>
+
+                        </div>
+
+                        <div class="dados-extras">
+                            <input type="text" placeholder="Caminho" name="caminho" required>
+                            <input type="date" name="entrega" required>
+                        </div>
     
 
                         <div class="botoes">
@@ -60,6 +82,12 @@
 
 
                 </div>
+                
+                <div class="logos">
+                    <a href="../pages/landingpage_creatorsync.html" target="_blank"><img src="../imgs/keyframe_logo_a.png"></a>
+                    <img src="../imgs/creatorsync_logo_a.png">
+                </div>
+
                 
             </section>
 
